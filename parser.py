22e0a63a9621bd45b19f8ec1e03e9cc11f816d85
ipowaletelegram <ipowalee@gmail.com>
2026-07-06 22:16:36 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,20 +5,13 @@ import re
 def clean(value):
     if value is None:
         return ""
-
     return " ".join(value.split()).strip()
 
 
 def extract_gmp(text):
-    """
-    Example:
-    ₹28 (13.08%)
-    """
-
     match = re.search(r"₹\s*([\d\.]+)\s*\((.*?)\)", text)
 
     if match:
-
         return {
             "gmp": match.group(1),
             "gain": match.group(2)
@@ -40,6 +33,7 @@ def parse_investorgain(html):
         raise Exception("reportTable not found")
 
     rows = table.select("tbody tr")
+
     print("Rows Found :", len(rows))
 
     ipos = []
@@ -53,42 +47,32 @@ def parse_investorgain(html):
 
         company = clean(cols[0].find("a").get_text())
 
-gmp_text = clean(cols[1].get_text(" ", strip=True))
+        gmp_text = clean(cols[1].get_text(" ", strip=True))
 
-gmp_data = extract_gmp(gmp_text)
+        gmp_data = extract_gmp(gmp_text)
 
-issue_price = clean(cols[4].get_text())
+        issue_price = clean(cols[4].get_text())
 
-lot = clean(cols[6].get_text())
+        lot = clean(cols[6].get_text())
 
-open_date = clean(cols[7].get_text())
+        open_date = clean(cols[7].get_text())
 
-close_date = clean(cols[8].get_text())
+        close_date = clean(cols[8].get_text())
 
-listing = clean(cols[10].get_text())
+        listing = clean(cols[10].get_text())
 
-updated = clean(cols[11].get_text())
+        updated = clean(cols[11].get_text())
 
         ipos.append({
-
             "company": company,
-
             "gmp": gmp_data["gmp"],
-
             "gain": gmp_data["gain"],
-
             "issue_price": issue_price,
-
             "lot": lot,
-
             "open": open_date,
-
             "close": close_date,
-
             "listing": listing,
-
             "updated": updated
-
         })
 
     return ipos
